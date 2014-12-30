ac8b58e0b4c0a61323bd008ef86d4b1d62d29551
Aleks Kamko <aykamko@gmail.com>
2014-12-29 18:42:21 -0800

    fixed prezto prompt

--- prompt_ayk_setup.zsh
+++ prompt_ayk_setup.zsh
@@ -6,24 +6,24 @@
 #
 #
 
-primary_color='cyan'
-secondary_color='blue'
+_primary_color='cyan'
+_secondary_color='blue'
 
 function +vi-git_remote {
     local left_right
     local -a remote_info
-    set -- $(command git rev-list --left-right --count @{u}.. 2> /dev/null)
+    set -- $(command git rev-list --left-right --count @\{u\}.. 2> /dev/null)
 
     # $1 is behind, $2 is forward
     if [[ -n $2 && $2 != 0 ]]; then
-        remote_info+=( '+$2' )
+        remote_info+=("+$2")
         left_right=true
     fi
     if [[ -n $1 && $1 != 0 ]]; then
-        (( $left_right )) && remote_info+=( ', ' )
-        remote_info+=( '-$1' )
+        (( $left_right )) && remote_info+=(", ")
+        remote_info+=("-$1")
     fi
-    [[ -n ${remote_info} ]] && hook_com[misc]='%F{$secondary_color}{%f%F{yellow}%{(j:/:)remote_info}%f%F{secondary_color}}%f'
+    [[ -n $remote_info ]] && hook_com[misc]="%F{$_secondary_color}{%f%F{yellow}${remote_info}%f%F{$_secondary_color}}%f"
 }
 
 function prompt_ayk_precmd {
@@ -47,17 +47,14 @@ function prompt_ayk_setup {
     zstyle ':vcs_info:*' check-for-changes true
     zstyle ':vcs_info:*' stagedstr '%F{yellow}●%f'
     zstyle ':vcs_info:*' unstagedstr '%F{green}●%f'
-    zstyle ':vcs_info:*' formats ' %F{$secondary_color}(%f%F{red}%b %f%c%u%F{$secondary_color})%f%m'
-    zstyle ':vcs_info:*' actionformats ' %F{$secondary_color}(%f%F{red}%b%f%c%u|%F{cyan}%a%f%F{$secondary_color})%f%m'
+    zstyle ':vcs_info:*' formats ' %F{$_secondary_color}(%f%F{red}%b%f%c%u%F{$_secondary_color})%f%m'
+    zstyle ':vcs_info:*' actionformats ' %F{$_secondary_color}(%f%F{red}%b%f%c%u|%F{cyan}%a%f%F{$_secondary_color})%f%m'
     zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b|%F{cyan}%r%f'
     zstyle ':vcs_info:git*+set-message:*' hooks git_remote
 
     # Define prompts.
-    PROMPT='%F{$primary_color}%3~%f${vcs_info_msg_0_} 🐼  '
+    PROMPT='%F{$_primary_color}%3~%f${vcs_info_msg_0_} 🐼  '
     RPROMPT=''
 }
-
-unset primary_color
-unset secondary_color
 
 prompt_ayk_setup "$@"
