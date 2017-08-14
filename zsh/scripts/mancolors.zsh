571256c03db81f0ba55f9bfcddcc9cdd147abb27
Aleks Kamko <aykamko@gmail.com>
2017-08-13 18:02:07 -0700

    update to tmux 2.5 and latest nvim

--- zsh/scripts/mancolors.zsh
+++ zsh/scripts/mancolors.zsh
@@ -1,11 +1,12 @@
-man() {
+_ayk_man() {
   env \
-  LESS_TERMCAP_mb=$(printf "\e[1;34m") \
-  LESS_TERMCAP_md=$(printf "\e[1;34m") \
-  LESS_TERMCAP_me=$(printf "\e[0m") \
-  LESS_TERMCAP_se=$(printf "\e[0m") \
-  LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-  LESS_TERMCAP_ue=$(printf "\e[0m") \
-  LESS_TERMCAP_us=$(printf "\e[1;37m") \
-  man "$@"
+    LESS_TERMCAP_mb=$(printf "\e[1;34m") \
+    LESS_TERMCAP_md=$(printf "\e[1;34m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;37m") \
+  command man "$@"
 }
+alias man=_ayk_man
