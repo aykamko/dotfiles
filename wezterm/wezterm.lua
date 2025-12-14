25b449d6455604edb8ead098ff1d839a43e57ebf
Aleks Kamko <akamko@figma.com>
2025-12-14 12:18:40 -0800

    various changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,8 +12,12 @@ if wezterm.config_builder then
 end
 
 config.leader = { key = 'Space', mods = 'CTRL', timeout_milliseconds = 1000 }
-config.keys = {
-  -- Create panes tmux-style
+config.keys = {-- Create panes tmux-style
+  {
+    -- Claude Code Shift+Enter
+    key = "Enter", mods = "SHIFT",
+    action = wezterm.action { SendString="\x1b\r" }
+  },
   {
     mods = 'LEADER', key = 'h',
     action = wezterm.action.SplitPane { direction = 'Left' },
@@ -88,5 +92,34 @@ config.keys = {
 
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE|MACOS_FORCE_DISABLE_SHADOW'
 config.color_scheme = 'Arthur (Gogh)'
+
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+local function tab_title(tab_info)
+    local title = tab_info.tab_title
+    -- if the tab title is explicitly set, take that
+    if title and #title > 0 then
+        return title
+    end
+    -- Otherwise, use the title from the active pane
+    -- in that tab
+    return tab_info.active_pane.title
+end
+
+-- function tab_bar.on_format_tab_title(tab, _tabs, _panes, _config, _hover, _max_width)
+--     local zoomed = ''
+--     local index = tab.tab_index + 1
+--     local title = tab_title(tab)
+--     if tab.active_pane.is_zoomed then
+--         zoomed = '[Z]'
+--     end
+--     return {
+--         { Text = string.format(' %d %s%s ', index, title, zoomed) }
+--     }
+-- end
+--
+-- wezterm.on('format-tab-title', tab_bar.on_format_tab_title)
 
 return config
