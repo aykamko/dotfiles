20001ea53011aa053bdf6e2f254eded869a0f98c
Aleks Kamko <akamko@figma.com>
2026-05-06 01:25:10 +0000

    fix profiler

--- bin/zsh-profile-processor.py
+++ bin/zsh-profile-processor.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import argparse
 import fileinput
 import re
@@ -9,7 +9,7 @@ parser.add_argument('zsh_start_time', type=float)
 parser.add_argument('profiler_output')
 args = parser.parse_args()
 
-time_re = re.compile('^\+(\d+\.\d+) (.*)')
+time_re = re.compile(r'^\+(\d+\.\d+) (.*)')
 
 sorted_lines = []
 
@@ -19,7 +19,7 @@ prev_time = zsh_start_time
 for line in fileinput.input(args.profiler_output, inplace=True):
     match = time_re.match(line)
     if not match:
-        print line,
+        print(line, end='')
         continue
 
     cur_line = match.group(2)
@@ -28,7 +28,7 @@ for line in fileinput.input(args.profiler_output, inplace=True):
     cumulative_time = cur_time - zsh_start_time
     line_time = cur_time - prev_time
 
-    print '+%011.8f [%011.8f] %s' % (cumulative_time, line_time, cur_line)
+    print('+%011.8f [%011.8f] %s' % (cumulative_time, line_time, cur_line))
 
     heapq.heappush(sorted_lines, (-line_time, prev_line))
 
@@ -38,8 +38,8 @@ for line in fileinput.input(args.profiler_output, inplace=True):
 # print 25 longest running lines to another file
 with open(args.profiler_output + '_max', 'w') as f:
     for _ in range(25):
-        time, line = heapq.heappop(sorted_lines)
-        time = (-time * 1000)
-        f.write('%011.8f: %s\n' % (time, line))
-        if not len(sorted_lines):
+        if not sorted_lines:
             break
+        neg_time, line = heapq.heappop(sorted_lines)
+        time_ms = -neg_time * 1000
+        f.write('%011.8f: %s\n' % (time_ms, line))
